c58ac95645eb96044982dbdeceac62ff10802dda
TANMAY3122 <tanmaykashyap3122@gmail.com>
2023-09-10 01:45:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+const API_KEY = "56c7d607cf61403ea0b8ccdf0fe505d3";
+const url = "https://newsapi.org/v2/everything?q=";
+
+window.addEventListener("load", () => fetchNews("India"));
+
+async function fetchNews(query) {
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const data = await res.json();
+    bindData(data.articles);
+    // console.log(data);
+}
+
+function bindData(articles) {
+    const cardsContainer = document.getElementById("cards-container");
+    const newsCardTemplate = document.getElementById("template-news-card");
+
+    cardsContainer.innerHTML="";
+
+    articles.forEach((article) => {
+        if (!article.urlToImage) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone, article);
+        cardsContainer.appendChild(cardClone);
+        
+    });
+
+}
+
+function fillDataInCard(cardClone, article) {
+    const newsImg = cardClone.querySelector('#news-img');
+    const newsTitle = cardClone.querySelector('#news-title');
+    const newsSource = cardClone.querySelector('#news-source');
+    const newsDesc = cardClone.querySelector('#news-desc');
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+
+    const date = new Date(article.publishedAt).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta"
+    });
+
+    newsSource.innerHTML = `${article.source.name} : ${date}`;
+
+    cardClone.firstElementChild.addEventListener("click", () => {
+        window.open(article.url, "_black");
+    });
+}
+
+let curSelectedNav = null;
+function onNavItemClick(id) {
+    fetchNews(id);
+    const navItem = document.getElementById(id);
+    curSelectedNav?.classList.remove('active');
+    curSelectedNav = navItem;
+    curSelectedNav.classList.add('active');
+
+
+        
+}
+
+const searchText = document.getElementById("search-text");
+searchText.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13){
+const query = searchText.value;
+if (!query) return;
+fetchNews(query);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav = null;
+
+// alert(searchText.value)
+    }
+})
